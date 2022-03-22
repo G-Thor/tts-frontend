2d1b393bab3a0c10eaa67d3ff737dd49734e3ab4
Anna Nikulásdóttir <anna@grammatek.com>
2022-03-22 10:29:27 +0000

    edit setup

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
 
     # check requirements!
     install_requires=[
-        'text-cleaner @ git+https://git@github.com@grammatek/text-cleaner@tts-frontend-dev',
+        'text-cleaner @ git+https://git@github.com/grammatek/text-cleaner@tts-frontend-dev',
         'regina_normalizer @ git+https://github.com/grammatek/regina_normalizer@tts-frontend',
         'reynir-correct @ git+https://github.com/grammatek/GreynirCorrect4LT@tts_frontend',
         'ice-g2p @ git+https://github.com/grammatek/ice-g2p',
